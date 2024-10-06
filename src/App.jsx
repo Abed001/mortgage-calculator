4a4455c35aad88e5f158d48466d78ca8ca165798
Abed001 <abed.alroufe@gmail.com>
2024-10-06 23:14:43 +0300

    finished the calculations

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import Illustrationsvg from "./components/Illustrationsvg"
 import Iconcalculator from "./components/Iconcalculator"
 
@@ -8,6 +8,30 @@ function App() {
 
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
   const onSubmit = data => console.log(data);
+
+  const mortgageAmount = watch('mortgageAmount');
+  const interestRate = watch('interestRate');
+  const mortgageTerm = watch('mortgageTerm');
+  const [repaymentType, setRepaymentType] = useState('repayment');
+  //const [clicked, setClicked] = useState(false);
+
+
+  const calculateRepayment = () => {
+    const principal = mortgageAmount;
+    const monthlyInterestRate = interestRate / 100 / 12;
+    const numberOfMonths = mortgageTerm * 12;
+
+    const monthlyRepayment = principal * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfMonths) / (Math.pow(1 + monthlyInterestRate, numberOfMonths) - 1);
+    return monthlyRepayment;
+  };
+
+  const calculateInterestOnly = () => {
+    const monthlyInterestRate = interestRate / 100 / 12;
+    const interestOnlyPayment = mortgageAmount * monthlyInterestRate;
+
+    return interestOnlyPayment;
+  };
+
 
   return (
     <div className='flex justify-center items-center w-full lg:h-[100vh]'>
@@ -27,8 +51,8 @@ function App() {
                   <div className='flex'>
                     <div><p className="font-semibold text-slate-700 bg-slate-300 p-2 h-[50px] lg:h-[40px] flex justify-center items-center">$</p></div>
                     <div className='flex flex-col w-[100%] '>
-                      <input className='lg:mb-2 w-[100%]' {...register('mortgageAmount', { required: true })} />
-                      <div> {errors.mortgageAmount && <p className='text-[12px] -ml-[25px]'> This field is required</p>}</div>
+                      <input type='number' className='lg:mb-2 w-[100%]' {...register('mortgageAmount', { required: true })} />
+                      <div> {errors.mortgageAmount && <p className='text-[12px] -ml-[25px] mt-2'> This field is required</p>}</div>
                     </div>
 
                   </div>
@@ -36,22 +60,35 @@ function App() {
                   <div className='w-[100%] mt-5 flex flex-col lg:flex-row justify-center lg:gap-x-5 items-center lg:justify-evenly '>
                     <div className='flex flex-col  w-[100%] lg:w-[50%] mb-5 lg:mb-0 '>
                       <label className='font-semibold text-[12px] text-slate-300 mb-2'>Mortgage Term</label>
-                      <div className='flex'>
-                        <input className='input-reverse w-[100%]' {...register('mortgageTerm', { required: true })} />
-                        {errors.mortgageTerm && <p>This field is required</p>}
-                        <div><p className="font-semibold text-slate-700 bg-slate-300 p-2 h-[50px] lg:h-[40px]">years</p></div>
+                      <div className='flex flex-col w-[100%] '>
+                        <div className='flex'>
+                          <input type='number' className='input-reverse w-[100%]' {...register('mortgageTerm', { required: true })} />
+                          <div>
+                            <p className="font-semibold text-slate-700 bg-slate-300 p-2 h-[50px] lg:h-[40px]">years</p>
+                          </div>
+                        </div>
+                        <div> {errors.mortgageTerm && <p className='text-[12px] -ml-[25px] pl-6 mt-2'> This field is required</p>}</div>
+
+
                       </div>
 
                     </div>
 
                     <div className='flex flex-col w-[100%] lg:w-[50%]'>
                       <label className='font-semibold text-[12px] text-slate-300 mb-2'>Interest Rate</label>
-                      <div className='flex'>
-                        <input className='input-reverse  w-[100%]' {...register('interestRate', { required: true })} />
-                        {errors.interestRate && <p>This field is required</p>}
-                        <div><p className="font-semibold text-slate-700 bg-slate-300 p-2 h-[50px] lg:h-[40px]">%</p></div>
-                      </div>
+                      <div className='flex flex-col w-[100%] '>
+                        <div className='flex'>
+                          <input type='number' className='input-reverse w-[100%]' {...register('interestRate', { required: true })} />
+                          <div>
+                            <p className="font-semibold text-slate-700 bg-slate-300 p-2 h-[50px] lg:h-[40px]">%</p>
+                          </div>
+                        </div>
+                        <div> {errors.interestRate && <p className='text-[12px] -ml-[25px] pl-6 mt-2'> This field is required</p>}</div>
 
+
+
+
+                      </div>
                     </div>
 
                   </div>
@@ -59,18 +96,24 @@ function App() {
                     <label className='font-semibold text-[12px] text-slate-300 mb-2'>Mortgage Type</label>
 
                     <div className='relative border-2 border-slate-300 rounded-sm h-[50px] lg:h-[40px]  p-2 flex justify-start items-center'>
-                      <input className='inputs inputss w-6 ' type="radio" />
+                      <input className='inputs inputss w-6 ' type="radio" value="repayment" checked={repaymentType === 'repayment'} {...register('repaymentType')}  onChange={() => setRepaymentType('repayment')}/>
                       <p className='ml-2 font-bold text-slate-800'>Repayment</p></div>
 
 
                     <div className='relative mt-2 border-2 border-slate-300 rounded-sm h-[50px] lg:h-[40px]  p-2 flex justify-start items-center'>
-                      <input className=' inputss inputs w-6' type="radio" />
+                      <input className=' inputss inputs w-6' type="radio" value="interestOnly" checked={repaymentType === 'interestOnly'} {...register('repaymentType')} onChange={() => setRepaymentType('interestOnly')} />
                       <p className='ml-2 font-bold text-slate-800'>Interest Only</p></div>
 
                   </div>
                 </div>
                 <div className='w-[100%] flex justify-center lg:justify-start'>
-                  <button className='mb-10 cursor-pointer mt-10 bg-lime w-[100%] lg:w-[80%] py-4 lg:py-2 rounded-full text-slate-800 font-bold flex justify-center items-center gap-x-2 ' type="submit" ><Iconcalculator />Calculate Repayments</button>
+                  <button onClick={() => {
+                    if (repaymentType === 'repayment') {
+                      console.log('Monthly Repayment:', calculateRepayment());
+                    } else {
+                      console.log('Interest Only Payment:', calculateInterestOnly());
+                    }
+                  }} className='mb-10 cursor-pointer mt-10 bg-lime w-[100%] lg:w-[80%] py-4 lg:py-2 rounded-full text-slate-800 font-bold flex justify-center items-center gap-x-2 ' type="submit" ><Iconcalculator />Calculate Repayments</button>
                 </div>
               </form>
 
@@ -78,11 +121,14 @@ function App() {
           </div>
         </div>
         <div className='p-10 pb-20 lg:pb-0 gap-y-5 lg:rounded-bl-[5rem] lg:rounded-tr-3xl lg:rounded-br-3xl bg-slate-800 flex flex-col justify-center items-center h-[100%]  w-[100%] lg:w-[500px]'>
+
           <Illustrationsvg />
           <h1 className='text-white font-bold'>Results shown here</h1>
           <p className='text-[12px] text-center text-slate-400'>Complete the form and click "calculate repayment"
             to see what your monthly  repayment would be.
           </p>
+
+
         </div>
       </div>
     </div >
